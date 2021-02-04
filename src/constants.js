2baf048bc5c216c08bae8c3e67393665b0185226
maneroto <maneroto@hotmail.com>
2021-02-04 15:05:08 -0600

    Change frequencies quantitites

--- src/constants.js
+++ src/constants.js
@@ -16,36 +16,125 @@ export const SHIPPING_PLAN = 0;
 export const SHIPPING_PRODUCTS = 100;
 
 export const LIST_PURE_PRODUCTS = [
-  {id: 1, name: 'Toallitas Húmedas', desc: 'Toallas que cuentan con nuestro Sello Ecocert, solo usamos 5 ingredientes, entre ellos el 98.9% es agua.', img: 'https://assets.website-files.com/600eff8cbf53c99e0ed39440/6010374763ee0f308073019b_toallitas-humedas.png', price: 40, isDiaper: false},
-  {id: 2, name: 'BioGel Limpiador', desc: 'A base de albaricoque y aloe vera, ideal para piel sensible. Si tu bebé no queda radiante y limpio no es NUNUU.', img: 'https://assets.website-files.com/600eff8cbf53c99e0ed39440/600f6fd4a9317798cc7aaa22_biogel-limpiador.png', price: 80, isDiaper: false},
+    {
+        id: 1,
+        name: 'Toallitas Húmedas',
+        desc:
+            'Toallas que cuentan con nuestro Sello Ecocert, solo usamos 5 ingredientes, entre ellos el 98.9% es agua.',
+        img:
+            'https://assets.website-files.com/600eff8cbf53c99e0ed39440/6010374763ee0f308073019b_toallitas-humedas.png',
+        price: 40,
+        isDiaper: false,
+    },
+    {
+        id: 2,
+        name: 'BioGel Limpiador',
+        desc:
+            'A base de albaricoque y aloe vera, ideal para piel sensible. Si tu bebé no queda radiante y limpio no es NUNUU.',
+        img:
+            'https://assets.website-files.com/600eff8cbf53c99e0ed39440/600f6fd4a9317798cc7aaa22_biogel-limpiador.png',
+        price: 80,
+        isDiaper: false,
+    },
 ];
 
 export const LIST_ALL_PRODUCTS = [
-  ...LIST_PURE_PRODUCTS,
-  {id: 3, name: 'Pañalera', desc: 'Pañalera con una capacidad de hasta 50 pañales y cuenta con bolsas internas para que coloques tus toallas y lociones para tu bebé.', img: 'https://assets.website-files.com/600eff8cbf53c99e0ed39440/600f6fe8b03ce546cfa8e959_panalera-thumb.png', price: 600, isDiaper: false},
-  {id: 4, name: 'Pañales', desc: 'Los mejores pañales para tu bebé', img: 'https://assets.website-files.com/600eff8cbf53c99e0ed39440/600f2700c612ede6e7cbf076_diaper-design-01.png', price: 1000, isDiaper: true},
-  // {id: 5, name: '', desc: '', img: '', price: 80, isDiaper: false},
-  // {id: 6, name: '', desc: '', img: '', price: 600, isDiaper: true},
+    ...LIST_PURE_PRODUCTS,
+    {
+        id: 3,
+        name: 'Pañalera',
+        desc:
+            'Pañalera con una capacidad de hasta 50 pañales y cuenta con bolsas internas para que coloques tus toallas y lociones para tu bebé.',
+        img:
+            'https://assets.website-files.com/600eff8cbf53c99e0ed39440/600f6fe8b03ce546cfa8e959_panalera-thumb.png',
+        price: 600,
+        isDiaper: false,
+    },
+    {
+        id: 4,
+        name: 'Pañales',
+        desc: 'Los mejores pañales para tu bebé',
+        img:
+            'https://assets.website-files.com/600eff8cbf53c99e0ed39440/600f2700c612ede6e7cbf076_diaper-design-01.png',
+        price: 1000,
+        isDiaper: true,
+    },
+    // {id: 5, name: '', desc: '', img: '', price: 80, isDiaper: false},
+    // {id: 6, name: '', desc: '', img: '', price: 600, isDiaper: true},
 ];
 
 export const LIST_ALL_STAGES = [
-  { id: 0, name: 'Etapa 1', target: 'De 2 a 4kg', equiv: 'equivale a 210 pañales', img: 'https://assets.website-files.com/600eff8cbf53c99e0ed39440/600f25502537b237647b2f2f_stage.svg' },
-  { id: 1, name: 'Etapa 2', target: 'De 3 a 6kg', equiv: 'equivale a 180 pañales', img: 'https://assets.website-files.com/600eff8cbf53c99e0ed39440/600f25502537b237647b2f2f_stage.svg' },
-  { id: 2, name: 'Etapa 3', target: 'De 6 a 9kg', equiv: 'equivale a 150 pañales', img: 'https://assets.website-files.com/600eff8cbf53c99e0ed39440/600f25502537b237647b2f2f_stage.svg' },
-  { id: 3, name: 'Etapa 4', target: 'De 9 a 12kg', equiv: 'equivale a 150 pañales', img: 'https://assets.website-files.com/600eff8cbf53c99e0ed39440/600f25502537b237647b2f2f_stage.svg' },
-  { id: 4, name: 'Etapa 5', target: 'De 12 a 15kg', equiv: 'equivale a 120 pañales', img: 'https://assets.website-files.com/600eff8cbf53c99e0ed39440/600f25502537b237647b2f2f_stage.svg' },
-  { id: 5, name: 'Etapa 6', target: '+14Kg', equiv: 'equivale a 120 pañales', img: 'https://assets.website-files.com/600eff8cbf53c99e0ed39440/600f25502537b237647b2f2f_stage.svg' },
+    {
+        id: 0,
+        name: 'Etapa 1',
+        target: 'De 2 a 4kg',
+        equiv: 'equivale a 210 pañales',
+        img:
+            'https://assets.website-files.com/600eff8cbf53c99e0ed39440/600f25502537b237647b2f2f_stage.svg',
+    },
+    {
+        id: 1,
+        name: 'Etapa 2',
+        target: 'De 3 a 6kg',
+        equiv: 'equivale a 180 pañales',
+        img:
+            'https://assets.website-files.com/600eff8cbf53c99e0ed39440/600f25502537b237647b2f2f_stage.svg',
+    },
+    {
+        id: 2,
+        name: 'Etapa 3',
+        target: 'De 6 a 9kg',
+        equiv: 'equivale a 150 pañales',
+        img:
+            'https://assets.website-files.com/600eff8cbf53c99e0ed39440/600f25502537b237647b2f2f_stage.svg',
+    },
+    {
+        id: 3,
+        name: 'Etapa 4',
+        target: 'De 9 a 12kg',
+        equiv: 'equivale a 150 pañales',
+        img:
+            'https://assets.website-files.com/600eff8cbf53c99e0ed39440/600f25502537b237647b2f2f_stage.svg',
+    },
+    {
+        id: 4,
+        name: 'Etapa 5',
+        target: 'De 12 a 15kg',
+        equiv: 'equivale a 120 pañales',
+        img:
+            'https://assets.website-files.com/600eff8cbf53c99e0ed39440/600f25502537b237647b2f2f_stage.svg',
+    },
+    {
+        id: 5,
+        name: 'Etapa 6',
+        target: '+14Kg',
+        equiv: 'equivale a 120 pañales',
+        img:
+            'https://assets.website-files.com/600eff8cbf53c99e0ed39440/600f25502537b237647b2f2f_stage.svg',
+    },
 ];
 
 export const LIST_ALL_DESIGNS = [
-  { id: 0, img: 'https://uploads-ssl.webflow.com/600eff8cbf53c99e0ed39440/60142e451bc89fe6659de3c4_Textura-Dinosaurios%403x.png'},
-  { id: 1, img: 'https://assets.website-files.com/600eff8cbf53c99e0ed39440/6010378b63ee0f8b8273020c_design2.png' },
-  { id: 2, img: 'https://assets.website-files.com/600eff8cbf53c99e0ed39440/6010378be1e192268d8c3e09_design3.png' },
+    {
+        id: 0,
+        img:
+            'https://uploads-ssl.webflow.com/600eff8cbf53c99e0ed39440/60142e451bc89fe6659de3c4_Textura-Dinosaurios%403x.png',
+    },
+    {
+        id: 1,
+        img:
+            'https://assets.website-files.com/600eff8cbf53c99e0ed39440/6010378b63ee0f8b8273020c_design2.png',
+    },
+    {
+        id: 2,
+        img:
+            'https://assets.website-files.com/600eff8cbf53c99e0ed39440/6010378be1e192268d8c3e09_design3.png',
+    },
 ];
 
 export const LIST_ALL_FREQUENCIES = [
-  { id: 1, period: '3 Semanas', quantity: '11 pañales al día'},
-  { id: 2, period: '4 Semanas', quantity: '9 pañales al día'},
-  { id: 3, period: '5 Semanas', quantity: '7 pañales al día'},
-  { id: 4, period: '6 Semanas', quantity: '6 pañales al día'},
+    { id: 1, period: '3 Semanas', quantity: '9 pañales al día' },
+    { id: 2, period: '4 Semanas', quantity: '7 pañales al día' },
+    { id: 3, period: '5 Semanas', quantity: '6 pañales al día' },
+    { id: 4, period: '6 Semanas', quantity: '5 pañales al día' },
 ];
